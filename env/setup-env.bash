df9a4df5732f3ed047e87cf373aa9a7b09329df6
wuwei <wuweiwhu@outlook.com>
2026-04-10 11:11:38 +0800

    Update env bash

--- env/setup-env.bash
+++ env/setup-env.bash
@@ -30,8 +30,8 @@ export TIMELOOP_BASE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")/.."; 
 # We should separate the following depending on which OS is running.
 # We should also create separate scripts for bash and zsh. At the moment this
 # script works with both shells.
-export LD_LIBRARY_PATH=$TIMELOOP_BASE_PATH/lib:/usr/local/lib/:$LD_LIBRARY_PATH
-export DYLD_FALLBACK_LIBRARY_PATH=$TIMELOOP_BASE_PATH/lib:/usr/local/lib/:$DYLD_FALLBACK_LIBRARY_PATH
+export LD_LIBRARY_PATH=$TIMELOOP_BASE_PATH/lib:/usr/local/lib/:/lib:$LD_LIBRARY_PATH
+export DYLD_FALLBACK_LIBRARY_PATH=$TIMELOOP_BASE_PATH/lib:/usr/local/lib/:/lib:$DYLD_FALLBACK_LIBRARY_PATH
 export PATH=$TIMELOOP_BASE_PATH/bin:$PATH
 export TIMELOOP_INCLUDE_PATH=$TIMELOOP_BASE_PATH/include
 export TIMELOOP_LIB_PATH=$TIMELOOP_BASE_PATH/lib
